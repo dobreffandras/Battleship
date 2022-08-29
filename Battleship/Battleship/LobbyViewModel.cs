c99199da0849cf1084ae79c8a64ce13d2f0cfaff
András Dobreff <dobreffandras@gmail.com>
2022-08-29 15:20:06 +0200

    Extract service

--- Battleship/Battleship/LobbyViewModel.cs
+++ Battleship/Battleship/LobbyViewModel.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using Battleship.Services;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.ObjectModel;
@@ -12,41 +13,16 @@ namespace Battleship
         private int selectedGameIndex;
 
         public LobbyViewModel()
-        {
-            var newGameId = Guid.NewGuid().ToString();
+        {   
             OpenGames = new ObservableCollection<string>();
             JoinGame = new JoinGameCommand(this);
-            SendOpenGameMessage(newGameId);
+            var communicationService = new CommunicationService();
+            communicationService.NewOpenGameCallback = this.OnNewOpenGame;
+            communicationService.Connect();
         }
 
-        private void SendOpenGameMessage(string newGameId)
+        private void OnNewOpenGame(string gameId)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" }; // TODO extract to config
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-
-            channel.QueueDeclare(
-                queue: "open_games", // TODO extract magic constant
-                durable: false,
-                exclusive: false,
-                autoDelete: false);
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += OpenGamesMessageReceived;
-            channel.BasicConsume(
-                queue: "open_games",
-                autoAck: false,
-                consumer: consumer);
-
-            channel.BasicPublish(
-                exchange: string.Empty,
-                routingKey: "open_games",
-                body: Encoding.UTF8.GetBytes(newGameId));
-        }
-
-        private void OpenGamesMessageReceived(object? sender, BasicDeliverEventArgs args)
-        {
-            var gameId = Encoding.UTF8.GetString(args.Body.ToArray());
             OpenGames.Add(gameId);
         }
 
---
+++ Battleship/Battleship/Services/CommunicationService.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Services
+{
+    internal class CommunicationService
+    {
+        private string newGameId;
+
+        public CommunicationService()
+        {
+            newGameId = Guid.NewGuid().ToString();
+        }
+
+        public void Connect()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // TODO extract to config
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "open_games", // TODO extract magic constant
+                durable: false,
+                exclusive: false,
+                autoDelete: false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += OpenGamesMessageReceived;
+            channel.BasicConsume(
+                queue: "open_games",
+                autoAck: false,
+                consumer: consumer);
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "open_games",
+                body: Encoding.UTF8.GetBytes(newGameId));
+        }
+
+        public Action<string>? NewOpenGameCallback { get; set; }
+
+        private void OpenGamesMessageReceived(object? sender, BasicDeliverEventArgs args)
+        {
+            var gameId = Encoding.UTF8.GetString(args.Body.ToArray());
+            NewOpenGameCallback?.Invoke(gameId);
+        }
+    }
+}
